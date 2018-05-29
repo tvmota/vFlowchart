2a992d9309b1c4c72f54c6071b270a22bdfe5c52
Tiago Vieira Mota <tvmota@eicon.com.br>
2018-05-29 19:27:04 -0300

    Versão finalizada com seleção e mudança de estilo do svg rect

--- src/components/flowchart/FlowChart.vue
+++ src/components/flowchart/FlowChart.vue
@@ -6,7 +6,9 @@ export default {
   data () {
     return {
       svgLayer: '',
-      etapas: []
+      etapas: [],
+      defs: '',
+      etapaSelecionada: ''
     }
   },
   methods: {
@@ -26,42 +28,150 @@ export default {
         .attr('id', 'svgLayer')
         .attr('width', '100%')
         .attr('height', self.$refs.flowContainer.offsetHeight)
+      self.defs = self.svgLayer.append('defs')
+      self.defs.append('marker')
+        .attr('id', 'arrow')
+        .attr('viewBox', '0 -5 10 10')
+        .attr('refX', 5)
+        .attr('refY', 0)
+        .attr('markerWidth', 4)
+        .attr('markerHeight', 4)
+        .attr('orient', 'auto')
+        .append('path')
+        .attr('class', 'arrowHead')
+        .attr('d', 'M0,-5L10,0L0,5')
       self.drawEtapa()
     },
     drawEtapa () {
       let self = this
       let texts = []
+      let etapas = self.fluxo.etapas.filter(etapa => etapa.tipo < 3)
 
-      self.fluxo.etapas.map((flx, idx) => {
-        
+      etapas.map((flx, idx) => {
         let txt = self.svgLayer.append('text')
-          .attr('x', self.svgLayer.node().width.baseVal.value * 0.3)
-          .attr('y', (self.svgLayer.node().height.baseVal.value * 0.2) * (idx + 1))
+          .attr('id', flx.id)
+          .attr('x', self.svgLayer.node().width.baseVal.value * 0.2)
+          .attr('y', (self.svgLayer.node().height.baseVal.value * 0.25) * (idx + 1))
           .attr('dy', '0em')
           .attr('text-anchor', 'middle')
+          .attr('data-tipo', flx.tipo)
+          .attr('data-id', flx.id)
           .style('font', '300 16px arial')
           .text(flx.nome)
-        if ((self.svgLayer.node().height.baseVal.value * 0.17) * (idx + 1) > self.svgLayer.node().height.baseVal.value) {
-          self.svgLayer.attr('height', (self.svgLayer.node().height.baseVal.value * 0.2) * (idx + 2))
+        if ((self.svgLayer.node().height.baseVal.value * 0.15) * (idx + 1) > self.svgLayer.node().height.baseVal.value) {
+          self.svgLayer.attr('height', (self.svgLayer.node().height.baseVal.value * 0.25) * (idx + 2))
         }
-        texts.push(txt);
+        texts.push(txt)
       })
 
-      texts.map((txt)=>{
+      texts.map((txt) => {
         let bbox = txt.node().getBBox()
         let etapa = self.svgLayer.append('rect')
+          .attr('id', `box${txt.node().getAttribute('id')}`)
           .attr('x', bbox.x - 15)
           .attr('y', bbox.y - 15)
+          .attr('data-tipo', Number(txt.node().getAttribute('data-tipo')))
+          .attr('data-id', Number(txt.node().getAttribute('data-id')))
           .attr('width', bbox.width + 30)
           .attr('height', bbox.height + 30)
-          .style('fill', '#ccc')
-          .style('fill-opacity', '.3')
-          .style('stroke', '#666')
-          .style('stroke-width', '1.5px')
+        switch (Number(txt.node().getAttribute('data-tipo'))) {
+          case -1:
+            etapa.attr('class', 'diagrama_fim')
+            etapa.attr('ry', 25)
+            break
+          case 1:
+            etapa.attr('class', 'diagrama_inicio')
+            etapa.attr('ry', 25)
+            document.getElementById(etapa.node().getAttribute('id')).addEventListener('click', (evt) => {
+              self.selectEtapa(evt.srcElement)
+            })
+            break
+          case 2:
+            etapa.attr('class', 'diagrama_etapa')
+            etapa.attr('ry', 10)
+            document.getElementById(etapa.node().getAttribute('id')).addEventListener('click', (evt) => {
+              self.selectEtapa(evt.srcElement)
+            })
+            break
+        }
         self.etapas.push(etapa)
       })
 
+      self.etapas.map((etapa, idx) => {
+        let proxEtapa = self.etapas[idx + 1]
+        let bbox = etapa.node().getBBox()
+        if (Number(etapa.node().getAttribute('data-tipo')) > 0) {
+          self.svgLayer.append('line')
+            .attr('class', 'diagrama_arrow')
+            .attr('marker-end', 'url(#arrow)')
+            .attr('x1', bbox.x + (bbox.width / 2))
+            .attr('x2', bbox.x + (bbox.width / 2))
+            .attr('y1', bbox.y + bbox.height)
+            .attr('y2', proxEtapa.node().getAttribute('y') - 5)
+        }
+      })
 
+      self.drawServico()
+    },
+    drawServico () {
+      let self = this
+      let servico = self.fluxo.etapas.filter(etapa => etapa.tipo > 2)
+      console.log(servico)
+
+      servico.map((serv) => {
+        let idEtapa = self.fluxo.links.filter(etapa => etapa.to === serv.id)[0]
+        let etapa = document.getElementById(idEtapa.from)
+
+        let txtSvg = self.svgLayer.append('text')
+          .attr('id', serv.id)
+          .attr('x', self.svgLayer.node().width.baseVal.value * 0.8)
+          .attr('y', etapa.getBBox().y + 15)
+          .attr('dy', '0em')
+          .attr('text-anchor', 'middle')
+          .attr('data-tipo', serv.tipo)
+          .style('font', '300 16px arial')
+          .text(serv.nome)
+
+        let boxServ = self.svgLayer.append('rect')
+          .attr('id', txtSvg.node().getAttribute('id'))
+          .attr('x', txtSvg.node().getBBox().x - 15)
+          .attr('y', txtSvg.node().getBBox().y - 15)
+          .attr('ry', 10)
+          .attr('data-tipo', Number(txtSvg.node().getAttribute('data-tipo')))
+          .attr('width', txtSvg.node().getBBox().width + 30)
+          .attr('height', txtSvg.node().getBBox().height + 30)
+
+        switch (serv.tipo) {
+          case 3:
+            boxServ.attr('class', 'diagrama_servico')
+            break
+          case 4:
+            boxServ.attr('class', 'diagrama_fluxo')
+            break
+          case 5:
+            boxServ.attr('class', 'diagrama_form')
+            break
+        }
+
+        etapa = document.getElementById(`box${idEtapa.from}`)
+        self.svgLayer.append('line')
+          .attr('class', 'diagrama_arrow')
+          .attr('x1', etapa.getBBox().x + etapa.getBBox().width)
+          .attr('x2', boxServ.node().getBBox().x)
+          .attr('y1', 25 + etapa.getBBox().y)
+          .attr('y2', 25 + etapa.getBBox().y)
+      })
+    },
+    selectEtapa (elm) {
+      let self = this
+      let classe = ''
+      if(self.etapaSelecionada){
+        classe = self.etapaSelecionada.getAttribute('class')
+        self.etapaSelecionada.setAttribute('class', classe.replace(/--.*/g, ''))
+      }
+      self.etapaSelecionada = elm
+      classe = self.etapaSelecionada.getAttribute('class')
+      self.etapaSelecionada.setAttribute('class', `${classe}--selecionado`)
     }
   },
   computed: {
